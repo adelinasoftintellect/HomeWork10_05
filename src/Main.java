9bec131ec40e2d1554f8f20c923ca125061050ad
Adi <adelina_yordanova11@abv.bg>
2016-06-11 17:17:54 +0300

    HomwWork10_05.

---
+++ src/Main.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter the expression: ");
+		String s = input.nextLine();
+		System.out.println(balancedParenthensies(s));
+	}
+
+	public static boolean balancedParenthensies(String s) {
+		Stack<Character> stack = new Stack<Character>();
+		for (int i = 0; i < s.length(); i++) {
+			char c = s.charAt(i);
+			if (c == '[' || c == '(' || c == '{') {
+				stack.push(c);
+			} else if (c == ']') {
+				if (stack.isEmpty())
+					return false;
+				if (stack.pop() != '[')
+					return false;
+
+			} else if (c == ')') {
+				if (stack.isEmpty())
+					return false;
+				if (stack.pop() != '(')
+					return false;
+
+			} else if (c == '}') {
+				if (stack.isEmpty())
+					return false;
+				if (stack.pop() != '{')
+					return false;
+			}
+		}
+		return stack.isEmpty();
+	}
+}
